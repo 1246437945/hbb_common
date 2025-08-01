f367377961c2a5af28c3d2c49f8ce36c0a2e3aeb
1246437945 <151977722+1246437945@users.noreply.github.com>
2025-08-01 10:27:46 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.tovpower.dpdns.org"];
-pub const RS_PUB_KEY: &str = "WitBi96AkZFqI44Jbi89HVxdTujUr71KM6Rpz5SG9fA=";
+pub const RS_PUB_KEY: &str = "AePEtObgWoIVUB5me6KN75WeskkVyN2I0EtLkPPcgQU=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
